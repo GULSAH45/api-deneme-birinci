6d272e12f7729e1ef9a4de47726f31d568c43386
GULSAH45 <178339362+GULSAH45@users.noreply.github.com>
2024-12-10 20:06:04 +0300

    all done

--- app.js
+++ app.js
@@ -39,3 +39,44 @@
     container.innerHTML = "<p>Veriler yüklenemedi.</p>";
   }
 })();
+
+// Search bar fonksiyonu
+document
+  .querySelector("#myInput")
+  .addEventListener("input", async function searchInput() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+
+    const searchQuery = this.value.toLowerCase();
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(searchQuery)
+    );
+
+    const userCards = filteredUsers
+      .map((user) => {
+        return `
+   <div class="card">
+     <h2>${user.name}</h2>
+     <div class="section">
+       <i class="fa fa-user"></i>
+       <span>User Name:</span> ${user.username}
+     </div>
+     <div class="section">
+       <i class="fa fa-envelope"></i>
+       <span>Email:</span> ${user.email}
+     </div>
+     <div class="section">
+       <i class="fa fa-phone"></i>
+       <span>Phone:</span> ${user.phone}
+     </div>
+
+<div class="section">
+    <a class="btn btn-danger" href="user-details.html?userId=${user.id}">More Information</a>
+</div>
+   </div>
+ `;
+      })
+      .join(""); // Tüm kartları birleştirmek için join eklenmeli.
+
+    document.querySelector("#user-container").innerHTML = userCards; // Sonuçları ekrana yazdırıyoruz.
+  });
